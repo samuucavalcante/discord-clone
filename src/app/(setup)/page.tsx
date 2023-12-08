f6b47909e3b4563a4d10c68c96b12b20e2c98381
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2023-12-08 10:10:32 -0300

    feat: ensure auth of application

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db"
+import { initialProfile } from "@/lib/initial-profile"
+import { redirect } from "next/navigation"
+
+export default async function SetupPage () {
+  const profile = await initialProfile()
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
+  if(server) return redirect(`/servers/${server.id}`)
+
+  return <div>Create Server</div>
+}
