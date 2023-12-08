3a49016157f5830d8cc4febc7a5c782cdd7e00a0
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2023-12-08 13:51:16 -0300

    style: add button X

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { UploadDropzone } from '@/lib/uploadthing'
 import '@uploadthing/react/styles.css'
+import { X } from 'lucide-react'
 import Image from 'next/image'
 
 type ErrorParams = { message: string }
@@ -27,6 +28,13 @@ export function FileUpload({ endpoint, onChange, value }: FileUploadProps) {
     return (
       <div className="relative h-20 w-20">
         <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     )
   }
