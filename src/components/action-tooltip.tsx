fe5303e30da2dbf801b33bcd8cbaeab8cad02b18
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2023-12-11 11:04:34 -0300

    feat: create navigation components and button actions

---
+++ src/components/action-tooltip.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { ReactNode } from 'react'
+
+type ActionTooltipProps = {
+  label: string
+  children: ReactNode
+  side?: 'top' | 'right' | 'bottom' | 'left'
+  align?: 'start' | 'center' | 'end'
+}
+
+export function ActionTooltip({
+  children,
+  label,
+  align,
+  side,
+}: ActionTooltipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
